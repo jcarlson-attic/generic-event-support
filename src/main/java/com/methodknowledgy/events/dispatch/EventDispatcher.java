f04977c2581e6c5401c34c696de961072bf3cc34
Jarrod Carlson <jarrod.carlson@gmail.com>
2008-11-21 00:22:31 +0000

    Externalized the workings of the EventDispatcher to a sub class so that implementations can be swapped out by the host application if desired.

--- src/main/java/com/methodknowledgy/events/dispatch/EventDispatcher.java
+++ src/main/java/com/methodknowledgy/events/dispatch/EventDispatcher.java
@@ -1,41 +1,103 @@
 package com.methodknowledgy.events.dispatch;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.methodknowledgy.events.Event;
+import com.methodknowledgy.events.dispatch.impl.SimpleEventDispatcher;
 import com.methodknowledgy.events.impl.ChangeEvent;
 
-public class EventDispatcher {
+public abstract class EventDispatcher {
 
-	private static List<Subscriber> instance = new ArrayList<Subscriber>();
+    private static EventDispatcher instance;
 
-	private EventDispatcher() {
-	}
+    /**
+     * Generally speaking, an application will only ever have one active
+     * EventDispatcher. When an EventDispatcher is created, it should generally
+     * register itself as the default by passing <code>true</code> to this
+     * constructor. However, since in some instances, applications may wish to
+     * have multiple EventDispatchers, this behavior can be customized.
+     * 
+     * @param setAsEventDispatcher
+     *            <code>true</code> if this instance of an EventDispatcher
+     *            should become the default EventDispatcher.
+     */
+    protected EventDispatcher(boolean setAsEventDispatcher) {
+        if (setAsEventDispatcher) {
+            setInstance(this);
+        }
+    }
 
-	public static void register(Subscriber subscription) {
-		instance.add(subscription);
-	}
+    /**
+     * @return The active EventDispatcher, or a new SimpleEventDispatcher if no
+     *         other EventDispatcher has been created.
+     */
+    public static final EventDispatcher getInstance() {
+        if (instance == null) {
+            setInstance(new SimpleEventDispatcher());
+        }
+        return instance;
+    }
 
-	public static void unregister(Subscriber subscription) {
-		instance.remove(subscription);
-	}
+    /**
+     * <p>
+     * An application can provide a customized implementation of the
+     * EventDispatcher, although the SimpleEventDispatcher will work for most
+     * small to medium sized applications.
+     * </p>
+     * 
+     * <p>
+     * As a side note, if an EventDispatcher instance already exists, all
+     * existing Subscriptions registered with it will be transferred to the new
+     * instance. In this way, new implementations can be swapped in at any time
+     * without fear of losing existing Subscriptions that may have occurred in
+     * advance of the new instance registration.
+     * </p>
+     * 
+     * @param instance
+     */
+    public static final void setInstance(EventDispatcher instance) {
+        if (EventDispatcher.instance != null) {
+            for (Subscriber subscription : getInstance().getSubscriptions()) {
+                instance.registerImpl(subscription);
+            }
+        }
+        EventDispatcher.instance = instance;
+    }
 
-	public static <E extends Event<?, ?>> void dispatch(E event) {
-		for (Subscriber subscription : instance) {
-		    if (subscription.handles(event)) {
-		        subscription.handle(event);
-		    }
-		}
-	}
+    public final void register(Subscriber subscription) {
+        registerImpl(subscription);
+    }
 
-	public static <S, T> void dispatch(S source, String propertyName, T oldValue, T newValue) {
-		dispatch(source, propertyName, oldValue, newValue, null);
-	}
+    public final void unregister(Subscriber subscription) {
+        unregisterImpl(subscription);
+    }
 
-	public static <S, T> void dispatch(S source, String propertyName, T oldValue, T newValue, String message) {
-		ChangeEvent<S, T> event = new ChangeEvent<S, T>(
-				source, propertyName, oldValue, newValue, message);
-		dispatch(event);
-	}
+    public final <E extends Event<?, ?>> void dispatch(E event) {
+        dispatchImpl(event);
+    }
+
+    public final <S, T> void dispatch(S source, String propertyName,
+            T oldValue, T newValue) {
+        dispatch(source, propertyName, oldValue, newValue, null);
+    }
+
+    public final <S, T> void dispatch(S source, String propertyName,
+            T oldValue, T newValue, String message) {
+        ChangeEvent<S, T> event = new ChangeEvent<S, T>(source, propertyName,
+                oldValue, newValue, message);
+        dispatch(event);
+    }
+
+    public final List<Subscriber> getSubscriptions() {
+        return getSubscriptionsImpl();
+    }
+
+    protected abstract void registerImpl(Subscriber subscription);
+
+    protected abstract void unregisterImpl(Subscriber subscription);
+
+    protected abstract List<Subscriber> getSubscriptionsImpl();
+
+    protected abstract <E extends Event<?, ?>> void dispatchImpl(E event);
+
 }
